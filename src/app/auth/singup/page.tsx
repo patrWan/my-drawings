046b7c714de492ee45672c149de2f62d0988a4eb
patrWan <patricio.gonzalez.camilo@gmail.com>
2024-04-19 21:02:34 -0400

    fix

--- src/app/auth/singup/page.tsx
+++ src/app/auth/singup/page.tsx
@@ -5,7 +5,7 @@ export const font = DM_Sans({ subsets: ["latin"] });
 
 export const runtime = 'edge';
 
-export default function page() {
+export default async function page() {
   return (
     <div className="h-auto m-auto w-96 grid gap-1 p-4 border-2 text-black bg-white border-black rounded-3xl">
         <p className={`${font.className} text-2xl font-bold text-center`}>
