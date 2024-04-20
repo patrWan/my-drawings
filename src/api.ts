f02c32d866b28633834ead9ac9564d6b205c9e21
patrWan <patricio.gonzalez.camilo@gmail.com>
2024-04-19 21:11:32 -0400

    remove unnused api functions

--- src/api.ts
+++ src/api.ts
@@ -5,40 +5,7 @@ import { login, logout } from "@/actions/loginAction";
 const api = {
   drawing: {
     list: async (): Promise<Drawing[]> => {
-      return [
-        {
-          id: 1,
-          date: "2024-04-10:13:00:00",
-          url: "/draw1.png",
-          description:
-            "If I told you that I loved you Tell me, what would you say?",
-          usernameId: "patr.wan",
-        },
-        {
-          id: 2,
-          date: "2024-04-10:17:00:00",
-          url: "/draw1.png",
-          description:
-            "Fallin' again, I need a pick-me-up I've been callin' you friend, I might need to give it up",
-          usernameId: "patr.wan",
-        },
-        {
-          id: 3,
-          date: "2024-04-11:20:00:00",
-          url: "/draw1.png",
-          description:
-            "I'm sick and I'm tired too I can admit, I am not fireproof I feel it burning me I feel it burning you I hope I don't murder me I hope I don't burden you If I do, I do",
-          usernameId: "patr.wan",
-        },
-        {
-          id: 4,
-          date: "2024-04-14:21:10:00",
-          url: "/draw1.png",
-          description:
-            "I don't wanna be alone Sharper the blade is The easier it scars your soul",
-          usernameId: "nanitastica",
-        },
-      ];
+      return [];
     },
     getDrawings: async (usernameId: string): Promise<Drawing[]> => {
       const drawings = (await api.drawing.list()).filter(drawing => drawing.usernameId === usernameId).sort((a, b) =>  new Date(b.date).getTime() - new Date(a.date).getTime());
@@ -48,46 +15,7 @@ const api = {
 
   user: {
     list: async (): Promise<User[]> => {
-      return [
-        {
-          id: 1,
-          username: "patr.wan",
-          password: "123456",
-          profile : {
-            picture : "/profile.png",
-            description : "My heart is saying I'm not caring no more ♥️",
-            socialMedia : [
-              {
-                name : "instagram",
-                url : "https://www.instagram.com/patr.wan/"
-              },
-              {
-                name : "x",
-                url : "https://twitter.com/GenshinImpact"
-              },
-              {
-                name : "facebook",
-                url : "https://twitter.com/GenshinImpact"
-              },
-            ]
-          }
-        },
-        {
-          id: 2,
-          username: "nanitastica",
-          password: "123456",
-          profile : {
-            picture : "/profile_nani.png",
-            description : "⭐I am not throwin' away my shot⭐",
-            socialMedia : [
-              {
-                name : "instagram",
-                url : "https://www.instagram.com/t_n.nani/"
-              },
-            ]
-          }
-        },
-      ];
+      return [];
     },
 
     getUser: async (username : string | unknown): Promise<User | undefined> => {
